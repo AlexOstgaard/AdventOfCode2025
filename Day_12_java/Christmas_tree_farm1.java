743ae7ea23e9b2c8540fd240addb0cbc110e532c
Alexander Ramm Østgaard <alexostgaard@gmail.com>
2025-12-12 23:20:36 +0100

    I didn't stand a chance at day 12...

---
+++ Day_12_java/Christmas_tree_farm1.java
@@ -0,0 +1,8 @@
+public class Christmas_tree_farm1 {
+    //I read the problem like 4 times, but quickly realized that I have no clue how to solve it. 
+    // I also have to study for my exams, so this will remain unsolved...
+    
+    public static void main(String[] args) {
+        System.out.println("Merry christmas!");
+    }
+}
---
+++ Day_12_java/Christmas_tree_farm2.java
@@ -0,0 +1,3 @@
+public class Christmas_tree_farm2 {
+    // I have to dissapoint you here, I have nothing to show for.
+}
